0c192cd3c543d5b8f0691b13fe2b73cae1d93e8a
Mohamed Naeem <116705611+Naeemx7@users.noreply.github.com>
2025-08-29 14:50:58 +0300

    Add initial website files

---
+++ script.js
@@ -0,0 +1,17 @@
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+// Show/hide scroll-to-top button based on scroll position
+window.onscroll = function() {
+  var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+  
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = 'block';
+  } else {
+    scrollToTopBtn.style.display = 'none';
+  }
+};
